6da1e2b2d8c45fb835e24e276477067f59363c7a
Adam Skimina <adam.skimina@ovoo.pl>
2017-06-22 16:33:36 +0200

    added InvokeTimeoutEventWrapper interfaces

---
+++ src/main/java/pl/ovoo/ss7/wrapper/cap/event/InvokeTimeoutEventWrapper.java
@@ -0,0 +1,10 @@
+package pl.ovoo.ss7.wrapper.cap.event;
+
+/**
+ * InvokeTimeoutEventWrapper
+ *
+ * @author adam.skimina@ovoo.pl
+ */
+public interface InvokeTimeoutEventWrapper extends EventWrapper{
+
+}
---
+++ src/main/java/pl/ovoo/ss7/wrapper/map/event/InvokeTimeoutEventWrapper.java
@@ -0,0 +1,15 @@
+/**
+ * InvokeTimeoutEventWrapper.java
+ *  
+ * @author adam.skimina@ovoo.pl
+ */
+package pl.ovoo.ss7.wrapper.map.event;
+
+/**
+ * InvokeTimeoutEventWrapper
+ * 
+ * @author adam.skimina@ovoo.pl
+ */
+public interface InvokeTimeoutEventWrapper extends MapEventWrapper {
+
+}
